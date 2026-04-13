a2a874315513164005d7eefbf84ffad63f5c1660
Chang Huan Lo <changhuan.lo@ucl.ac.uk>
2026-04-13 14:52:45 +0100

    Set base URL (#7)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,7 +97,7 @@ html_title = "Sphinx-deployment-test"
 # Redirect the webpage to another URL
 # Sphinx will create the appropriate CNAME file in the build directory
 # https://www.sphinx-doc.org/en/master/usage/extensions/githubpages.html
-# html_baseurl = 'TODO: add your URL here'
+html_baseurl = "https://movement.neuroinformatics.dev"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
